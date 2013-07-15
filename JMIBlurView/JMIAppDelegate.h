32a04c8796eb550968c08e18e1dd88721652ab67
Jose Ibanez <jose.ibanez@gmail.com>
2013-07-15 19:56:33 -0400

    Initial Commit

---
+++ JMIBlurView/JMIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JMIAppDelegate.h
+//  JMIBlurView
+//
+//  Created by Jose Ibanez on 7/15/13.
+//  Copyright (c) 2013 Jose Ibanez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JMIBlurView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JMIBlurView
+//
+//  Created by Jose Ibanez on 7/15/13.
+//  Copyright (c) 2013 Jose Ibanez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JMIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JMIAppDelegate class]));
+    }
+}
